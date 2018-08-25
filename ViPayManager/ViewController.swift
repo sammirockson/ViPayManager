e6fdc3aed45c776d2c82478e1ad68d00bb1fe3a6
Samuel Rockson <you@yourdomain.com>
2018-08-25 21:15:10 +0800

    Initial Commit

---
+++ ViPayManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViPayManager
+//
+//  Created by Rock on 2018/8/25.
+//  Copyright © 2018 RockzAppStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
